74ecaa97274b8ffff355bc23975f76daff7cd150
Luis Ovalle <luispalp@hotmail.com>
2018-09-03 14:02:20 -0600

    Se hizo el diseño y se implemento la la clase cancion

--- app/src/main/java/com/ed2/lab0/Lab0.java
+++ app/src/main/java/com/ed2/lab0/Lab0.java
@@ -2,12 +2,42 @@ package com.ed2.lab0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
 
 public class Lab0 extends AppCompatActivity {
+
+    Button btnBuscar;
+    TextView lbBuscar;
+    EditText tbxBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_lab0);
+
+        btnBuscar = (Button) findViewById(R.id.btn1);
+        lbBuscar = (TextView) findViewById(R.id.lb1);
+        tbxBuscar = (EditText) findViewById(R.id.tbx1);
+        HashMap<String, cancion> diccionario = new HashMap<String, cancion>();
+        cancion nuevaCancion = null;
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Mensaje", Toast.LENGTH_LONG).show();
+                lbBuscar.setText(tbxBuscar.getText());
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/ed2/lab0/cancion.java
@@ -0,0 +1,12 @@
+package com.ed2.lab0;
+
+public class cancion {
+
+    public cancion(String nombre, String artista, Double duracion )
+    {
+        String Nombre = nombre;
+        String Artista = artista;
+        Double Duracion = duracion;
+    }
+
+}
