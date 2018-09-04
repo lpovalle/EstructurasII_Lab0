53a67a5bb4827d56e5481498fa8ae110b3a29d65
Luis Ovalle <luispalp@hotmail.com>
2018-09-03 20:53:29 -0600

    Se trabajo en el funcionamiento del diccionario

--- app/src/main/java/com/ed2/lab0/Lab0.java
+++ app/src/main/java/com/ed2/lab0/Lab0.java
@@ -1,7 +1,7 @@
 package com.ed2.lab0;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,14 +26,39 @@ public class Lab0 extends AppCompatActivity {
         btnBuscar = (Button) findViewById(R.id.btn1);
         lbBuscar = (TextView) findViewById(R.id.lb1);
         tbxBuscar = (EditText) findViewById(R.id.tbx1);
-        HashMap<String, cancion> diccionario = new HashMap<String, cancion>();
-        cancion nuevaCancion = null;
+        final HashMap<String, cancion> diccionario = new HashMap<String, cancion>();
+        final cancion nuevaCancion = null;
+
+        diccionario.put("When I'm gone", new cancion("When I'm gone","EMINEM", "Rap/Hip-Hop"));
+        diccionario.put("99 problems", new cancion("99 problems","Jay Z", "Rap/Hip-Hop"));
+        diccionario.put("Lose yourself", new cancion("Lose yourself","EMINEM", "Rap/Hip-Hop"));
+        diccionario.put("Suga suga", new cancion("Suga suga","Baby Bash", "Rap/Hip-Hop"));
+        diccionario.put("In da club", new cancion("In da club","50 cent", "Rap/Hip-Hop"));
+
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Mensaje", Toast.LENGTH_LONG).show();
-                lbBuscar.setText(tbxBuscar.getText());
+
+               String cancionbuscada = tbxBuscar.getText().toString();
+               cancion cancionencontrada;
+
+                try
+                {
+                    if(diccionario.containsKey(cancionbuscada))
+                    {
+                        cancionencontrada = diccionario.get(cancionbuscada);
+                        Toast.makeText(getApplicationContext(), cancionencontrada.Artista ,Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "No existe",Toast.LENGTH_LONG).show();
+                    }
+
+                }catch (Exception ex){
+
+                }
             }
         });
 
--- app/src/main/java/com/ed2/lab0/cancion.java
+++ app/src/main/java/com/ed2/lab0/cancion.java
@@ -2,11 +2,15 @@ package com.ed2.lab0;
 
 public class cancion {
 
-    public cancion(String nombre, String artista, Double duracion )
+    public String Nombre;
+    public String Artista;
+    public String Genero;
+
+    public cancion(String nombre, String artista, String genero )
     {
-        String Nombre = nombre;
-        String Artista = artista;
-        Double Duracion = duracion;
+        Nombre = nombre;
+        Artista = artista;
+        Genero = genero;
     }
 
 }
